7dffc848c129672f74d1c97b08b2c2218b524723
Stephen Jayakar <sjayakar@plaid.com>
2020-09-12 13:16:25 -0700

    moved to one file and started using use-package

--- stephen.el
+++ stephen.el
@@ -1,4 +1,99 @@
-(load-file "~/.emacs.d/stephen-init.el")
+;; Fixing path to mirror the one of my zsh shell
+(let ((path (shell-command-to-string ". ~/.zshrc; echo -n $PATH")))
+  (setenv "PATH" path)
+  (setq exec-path
+        (append
+         (split-string-and-unquote path ":")
+         exec-path)))
+;; setting emacs garbage collection threshold to a modern device level
+(setq gc-cons-threshold 100000000)
+
+;; package setup
+(require 'package)
+(package-initialize)
+
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.org/packages/") t)
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;  PACKAGES ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(use-package go-mode
+  :ensure t
+)
+
+(use-package tide
+  :ensure t
+)
+
+(use-package web-mode
+  :ensure t
+)
+
+(use-package rust-mode
+  :ensure t
+)
+
+(use-package lsp-mode
+  :ensure t
+  :commands (lsp lsp-deferred)
+  :hook (go-mode . lsp-deferred)
+  :config (
+    setq lsp-rust-server 'rust-analyzer
+  )
+)
+
+(use-package lsp-ui
+  :ensure t
+  :commands lsp-ui-mode
+)
+
+(use-package ivy
+  :ensure t
+  :config (
+    ivy-mode 1
+  )
+)
+
+(use-package undo-tree
+  :ensure t
+  :config (
+    global-undo-tree-mode
+  )
+)
+
+(use-package restart-emacs
+  :ensure t
+)
+
+(use-package flycheck
+  :ensure t
+)
+
+(use-package company
+  :ensure t
+  :config
+  ;; Optionally enable completion-as-you-type behavior.
+  (setq company-idle-delay 0)
+  (setq company-minimum-prefix-length 1)
+)
+
+(use-package company-lsp
+  :ensure t
+)
+
+(use-package fzf
+  :ensure t
+)
+
+(use-package projectile
+  :ensure t
+  :config (
+    projectile-mode +1
+  )
+)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; Custom Functions
 (defun copy-region-to-clipboard-mac ()
@@ -6,11 +101,7 @@
   (interactive)
   (shell-command-on-region (region-beginning) (region-end) "pbcopy"))
 
-;; Projectile Config
-(projectile-mode +1)
-(define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
 (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
-;; End Projectile Config
 
 ;; Moving backup files out of working directory
 (setq backup-directory-alist
@@ -18,46 +109,16 @@
 (setq auto-save-file-name-transforms
       `((".*" ,temporary-file-directory t)))
 
-;; UI
-(load-file "~/.emacs.d/stephen-ui.el")
-
-(global-undo-tree-mode)
-
-;; LSP MODE
-
-(setq lsp-keymap-prefix "C-x C-k")
-(add-hook 'web-mode-hook #'lsp)
-(add-hook '-mode-hook #'lsp)
-(setq lsp-rust-server 'rust-analyzer)
-
-;; Set up before-save hooks to format buffer and add/delete imports.
-;; Make sure you don't have other gofmt/goimports hooks enabled.
-(defun lsp-go-install-save-hooks ()
-  (add-hook 'before-save-hook #'lsp-format-buffer t t)
-  (add-hook 'before-save-hook #'lsp-organize-imports t t))
-(add-hook 'go-mode-hook #'lsp-go-install-save-hooks)
-
-;; END LSP MODE
-
-(ivy-mode 1)
-
-(add-hook 'term-mode-hook
-   (lambda ()
-     ;; C-x is the prefix command, rather than C-c
-     (term-set-escape-char ?\C-x)
-     (define-key term-raw-map "\M-y" 'yank-pop)
-     (define-key term-raw-map "\M-w" 'kill-ring-save)))
-(exec-path-from-shell-initialize)
-
-(with-eval-after-load 'flycheck
-  (add-hook 'flycheck-mode-hook #'flycheck-inline-mode))
-
 (setq custom-file "~/.emacs.d/garbage.el")
 
-;; Keybinds
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;  KEYBINDS ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (global-set-key (kbd "C-j") 'newline-and-indent)
 (global-set-key (kbd "C-x u") 'undo-tree-undo)
 (global-set-key (kbd "M-h") 'backward-kill-word)
+;; TODO: somehow only bind this on Mac OSX
 (global-set-key (kbd "C-c C-c") 'copy-region-to-clipboard)
 (global-set-key (kbd "C-x C-j") 'previous-buffer)
 (global-set-key (kbd "C-x C-l") 'next-buffer)
@@ -66,6 +127,33 @@
 (global-set-key (kbd "C-x C-r") 'revert-buffer)
 (global-set-key (kbd "C-x C-e") 'eval-buffer)
 (global-set-key (kbd "C-c RET") 'yafolding-toggle-element)
-;; (define-key yafolding-mode-map (kbd "C-c <C-RET>") 'yafolding-toggle-element)
-;; END Keybinds
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;  UI ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Display line numbers
+(global-linum-mode)
+(setq linum-format "%d ")
+
+;; Type 'y' for yes and 'n' for no
+(fset 'yes-or-no-p 'y-or-n-p)
+
+(setq-default c-basic-offset 2)
+;; Whitespace
+(setq-default show-trailing-whitespace t)
+
+;; Loading my custom theme, and telling Emacs it's safe.
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("0060308491490f94b8ce88b1c61a3a4d0ca205b7ee83ea3ac872d54045b135ed" default)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(load-theme 'fuck)
