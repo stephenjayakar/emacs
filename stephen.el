0bdeba9cab8bfbdfef9a481d0e33d0f48a6a96bb
Stephen Jayakar <sjayakar@plaid.com>
2022-02-18 10:38:29 -0800

    this is a fucking mess

--- stephen.el
+++ stephen.el
@@ -163,6 +163,49 @@
   )
 )
 
+(use-package ejira
+  :load-path "ejira/"
+  :ensure nil
+  :init
+  (setq jiralib2-url              "https://jira.plaid.com"
+        jiralib2-auth             'basic
+        jiralib2-user-login-name  "sjayakar"
+        jiralib2-token            nil
+        ejira-projects '("DEVX")
+
+        ejira-org-directory       "~/jira"
+        ejira-priorities-alist    '(("Highest" . ?A)
+                                    ("High"    . ?B)
+                                    ("Medium"  . ?C)
+                                    ("Low"     . ?D)
+                                    ("Lowest"  . ?E))
+        ejira-todo-states-alist   '(("To Do"       . 1)
+                                    ("In Progress" . 2)
+                                    ("Done"        . 3)))
+  :config
+    ;; Tries to auto-set custom fields by looking into /editmeta
+  ;; of an issue and an epic.
+  (add-hook 'jiralib2-post-login-hook #'ejira-guess-epic-sprint-fields)
+  (setq )
+
+  ;; They can also be set manually if autoconfigure is not used.
+  ;; (setq ejira-sprint-field       'customfield_10001
+  ;;       ejira-epic-field         'customfield_10002
+  ;;       ejira-epic-summary-field 'customfield_10004)
+
+  (require 'ejira-agenda)
+
+  ;; Make the issues visisble in your agenda by adding `ejira-org-directory'
+  ;; into your `org-agenda-files'.
+  (add-to-list 'org-agenda-files ejira-org-directory)
+
+  ;; Add an agenda view to browse the issues that
+  (org-add-agenda-custom-command
+   '("j" "My JIRA issues"
+     ((ejira-jql "resolution = unresolved and assignee = currentUser()"
+                 ((org-agenda-overriding-header "Assigned to me")))))))
+
+
 ;; Trying out another window switching paradigm
 (global-set-key (kbd "C-<tab>") 'other-window)
 (global-set-key (kbd "C-S-<tab>") 'previous-window-any-frame)
@@ -207,6 +250,8 @@
     ("C-c p" . projectile-command-map)
 )
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(setq-default org-id-track-globally t)
 
 (setq-default tab-width 4)
 (setq-default indent-tabs-mode nil)
