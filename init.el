da047a148cdf86e12cf3bd0b9537e0776823a110
Stephen Jayakar <aj12ay@gmail.com>
2026-03-28 14:08:01 -0700

    init

---
+++ init.el
@@ -0,0 +1,13 @@
+;;; init.el --- Load Stephen's Emacs config -*- lexical-binding: t; -*-
+
+;;; Commentary:
+
+;; Keep Emacs startup conventions standard by loading the primary config from
+;; stephen.el.
+
+;;; Code:
+
+(load-file (expand-file-name "stephen.el" user-emacs-directory))
+
+(provide 'init)
+;;; init.el ends here
